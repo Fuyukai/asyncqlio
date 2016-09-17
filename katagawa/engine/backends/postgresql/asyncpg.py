a4311a69b6551a484ffe0ed505f6b26e60a46d02
SunDwarf <sun@veriny.tf>
2016-09-17 23:28:44 +0100

    Add transaction base for asyncpg.

--- katagawa/engine/backends/postgresql/asyncpg.py
+++ katagawa/engine/backends/postgresql/asyncpg.py
@@ -1,9 +1,12 @@
 """
 An engine using the ``asyncpg`` backend.
 """
+import typing
+
 import asyncpg
 
 from katagawa.engine.base import BaseEngine
+from katagawa.engine.transaction import Transaction
 
 
 class _FakeContextManager:
@@ -15,6 +18,74 @@ class _FakeContextManager:
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
         return
+
+
+def get_param_query(sql: str, params: dict) -> typing.Tuple[str, tuple]:
+    """
+    Gets a parametrized query.
+
+    Returns the reparsed SQL, and a tuple of items to be passed as parameters.
+    """
+    # Dump the params into key -> value pairs.
+    kv = [(k, v) for (k, v) in params.items()]
+
+    # Define the list of items that are used to return.
+    items = []
+
+    fmt_dict = {}
+
+    # Iterate over the key-values, adding each key to the fmt_dict with the number index.
+    for n, (k, v) in enumerate(kv):
+        # Add to the format dict.
+        fmt_dict[k] = "${}".format(n)
+        items.append(v)
+
+    sql_statement = sql.format(**fmt_dict)
+
+    return sql_statement, tuple(items)
+
+
+class AsyncpgTransaction(Transaction):
+    """
+    A transaction class specific to the asyncpg driver.
+    """
+
+    async def execute(self, sql: str, params: dict):
+        """
+        Executes SQL inside the connection.
+        """
+        if not self.started:
+            raise RuntimeError("Cannot execute SQL inside non-started transaction")
+
+        # Create tuples from the params.
+        sql, args = get_param_query(sql, params)
+        # Execute the query.
+        result = await self.connection.fetch(sql, *args)
+
+        return result
+
+    def __init__(self, engine: 'AsyncpgEngine', isolation="read_committed", read_only=False, deferrable=False):
+        super().__init__(engine, read_only=read_only)
+
+        self.connection = engine.get_connection()
+
+        # Define the asyncpg transaction object.
+        self.internal_transaction = self.connection.transaction(isolation=isolation, readonly=read_only,
+                                                                deferrable=deferrable)
+
+    async def _acquire(self):
+        # Start the transaction.
+        await self.internal_transaction.start()
+        return self
+
+    async def _release(self):
+        # Rollback if we're read-only, otherwise commit.
+        if self.read_only:
+            await self.internal_transaction.rollback()
+        else:
+            await self.internal_transaction.commit()
+
+        return self
 
 
 class AsyncpgEngine(BaseEngine):
@@ -40,6 +111,9 @@ class AsyncpgEngine(BaseEngine):
             r = self.pool.acquire()
 
         return r
+
+    def emit_param(self, name: str):
+        return "{%s}" % name
 
     async def fetch(self, sql: str, rows: int = 1, params: dict = None):
         async with self.get_connection() as conn:
