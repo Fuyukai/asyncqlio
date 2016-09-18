14869bd8c58a393caec488e95d51c282ccf23d0d
SunDwarf <sun@veriny.tf>
2016-09-18 15:41:23 +0100

    Add aliased ABC for tokens with an alias.

--- katagawa/sql/__init__.py
+++ katagawa/sql/__init__.py
@@ -9,6 +9,9 @@ class Token(abc.ABC):
     """
     Base class for a token.
     """
+
+    __slots__ = ()
+
     def __init__(self, subtokens: typing.List['Token']):
         """
         :param subtokens: Any subtokens this token has.
@@ -19,6 +22,8 @@ class Token(abc.ABC):
     def name(self):
         """
         Returns the name of the token.
+
+        This is a unique identifier, but is not always related to the actual SQL underneath it.
         """
 
     @abc.abstractmethod
@@ -27,3 +32,20 @@ class Token(abc.ABC):
         Generate SQL from this statement.
         :return: The generated SQL.
         """
+
+
+class Aliased(Token):
+    """
+    Mixin class for an aliased token.
+    """
+
+    __slots__ = ("alias",)
+
+    def __init__(self, subtokens: typing.List['Token'], alias: str):
+        """
+        :param subtokens: Any subtokens this token has.
+        :param alias: The alias this token has.
+        """
+        super().__init__(subtokens)
+
+        self.alias = alias
