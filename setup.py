88ce73d196b35e6b19e080ba57647fff1519241c
Laura <l@veriny.tf>
2017-04-26 18:12:10 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+    name='katagawa',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://github.com/SunDwarf/Katagawa',
+    license='MIT',
+    author='Laura Dickinson',
+    author_email='l@veriny.tf',
+    description='An asyncio ORM for Python 3.5'
+)
