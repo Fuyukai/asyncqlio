e33d44ca880f31c93dc5eef8d5162fec71f8d090
Laura <l@veriny.tf>
2017-04-30 10:45:16 +0100

    Add PostgresqlDialect.

--- katagawa/backends/postgresql/__init__.py
+++ katagawa/backends/postgresql/__init__.py
@@ -1,5 +1,19 @@
 # used for namespace packages
 from pkgutil import extend_path
+
 __path__ = extend_path(__path__, __name__)
 
+from katagawa.backends.base import BaseDialect
+
 DEFAULT_CONNECTOR = "asyncpg"
+
+
+class PostgresqlDialect(BaseDialect):
+    """
+    The dialect for Postgres.
+    """
+    def has_checkpoints(self):
+        return True
+
+    def has_serial(self):
+        return True
