f7836e49c93476f556956836e2aa65acb80da8e8
Laura <l@veriny.tf>
2017-04-30 10:28:18 +0100

    Add automatic version scanning in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 import sys
-
+import re
 from distutils.core import setup
 from setuptools import find_packages
 
 if sys.version_info[0:2] < (3, 5):
     raise RuntimeError("This package requires Python 3.5+.")
 
+with open("katagawa/__init__.py") as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
 setup(
     name='katagawa',
-    version='0.1.0',
+    version=version,
     packages=find_packages(),
     url='https://github.com/SunDwarf/Katagawa',
     license='MIT',
