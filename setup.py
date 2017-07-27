be8a02c766e056ab7a049b982025de2df5211e1c
Laura <l@veriny.tf>
2017-07-27 16:57:31 +0100

    Update setup.py
    Signed-off-by: Laura <l@veriny.tf>

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import re
 import sys
 from distutils.core import setup
+from pathlib import Path
 
 if sys.version_info[0:2] < (3, 5):
     raise RuntimeError("This package requires Python 3.5+.")
@@ -26,10 +27,13 @@ setup(
     license='MIT',
     author='Laura Dickinson',
     author_email='l@veriny.tf',
-    description='An asyncio ORM for Python 3.5',
+    description='An asyncio ORM for Python 3.5+',
+    long_description=Path(__file__).with_name("README.rst").read_text(encoding="utf-8"),
     install_requires=[
+        "setuptools",
         "cached_property==1.3.0"
     ],
+    python_requires=">=3.5.2",
     entry_points={
         'console_scripts': ['asql-migrate=asyncqlio.orm.ddl.migration_tool:cli']
     }
